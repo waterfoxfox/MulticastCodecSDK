6d19db1b5bf72c906b333fab600bbed2b2e96882
gol_ele <mediapro@126.com>
2022-04-28 16:49:19 +0800

    v1.0.0

---
+++ README.md
@@ -0,0 +1,189 @@
+﻿
+
+---
+
+# 极简音视频组播(内置编解码) WIN SDK
+
+
+作为一种与单播（Unicast）和广播（Broadcast）并列的通信方式，组播（Multicast）技术能够有效地解决单点发送、多点接收的问题，从而实现了网络中点到多点的高效数据传送，能够节约大量网络带宽、降低网络负载。常见基于组播的音视频应用有同屏教学、校园广播等。组播基于UDP传输，因此同样面临UDP传输常见的乱序、丢包、重复等挑战，本SDK主要为传统UDP组播增加FEC前向纠错、NACK丢包重传、Qos、JitterBuff等功能，同时内置了音视频编解码能力，为开发音视频组播应用提供易用、可靠的解决方案。
+
+本音视频组播SDK，支持特性：
+* 1、	独有的NACK组播重传技术，为每个组播接收端提供个性化重传服务，网络适应性更强。
+* 2、	支持基于RS编码的FEC前向纠错，专为音视频类应用量身定制。
+* 3、	支持FEC动态Group分组，避免分组跨帧同时尽可能提升抗连续丢包能力。
+* 4、	发送端支持Smooth平滑发送，对视频大I帧场景进行优化。
+* 5、	支持接收端自适应JitterBuff缓存技术。
+* 6、	SDK支持输入YUV420P、NV12、NV21、RGB24、RGBA32格式原始图像，支持Nvidia、Qsv硬件H264编码以及X264软编码、X265软编码。当系统不支持硬编码时自行切换为软编码。
+* 7、   SDK支持ffmpeg H264、HEVC软件解码，输出YUV420P。
+* 8、   SDK支持输入输出音频PCM，采用Fdk AAC LC编解码。
+* 9、   SDK内置视频缩放、音频重采样能力，支持外层输入可变的不同于编码宽高的采集宽高、支持输出外层指定的采样率、声道数。
+* 10、	C++开发，性能强劲，无外部库依赖，支持32位、64位系统。
+* 11、	API接口简洁易用、完善的文档与DEMO参考。
+
+---
+
+
+
+# 一、基本概念
+单播、广播、组播：
+作为一种与单播（Unicast）和广播（Broadcast）并列的通信方式，组播（Multicast）技术能够有效地解决单点发送、多点接收的问题，从而实现了网络中点到多点的高效数据传送，能够节约大量网络带宽、降低网络负载。利用组播技术可以方便地提供一些新的增值业务，包括在线直播、网络电视、远程教育、远程医疗、网络电台、实时视频会议等对带宽和数据交互的实时性要求较高的信息服务。
+三种信息传输方式的比较
+* **单播** <br>
+如下图所示，在IP网络中若采用单播的方式，信息源（即Source）要为每个需要信息的主机（即Receiver）都发送一份独立的信息拷贝。采用单播方式时，网络中传输的信息量与需要该信息的用户量成正比，因此当需要该信息的用户数量较大时，信息源需要将多份内容相同的信息发送给不同的用户，这对信息源以及网络带宽都将造成巨大的压力。从单播方式的信息传播过程可以看出，该传输方式不利于信息的批量发送。单播主要用于公网场景或无线局域网中，这类场景中广播与组播均无法应用或效果不佳。<br>
+   ![在这里插入图片描述](https://img-blog.csdnimg.cn/6acec096dfcb44769a6dc720479bb017.png#pic_center) 
+ 
+											
+
+
+* **广播** <br>
+如下图所示，在一个网段中若采用广播的方式，信息源（即Source）将把信息传送给该网段中的所有主机，而不管其是否需要该信息。假设只有Host B、Host D和Host E需要信息，若将该信息在网段中进行广播，则原本不需要信息的Host A和Host C也将收到该信息，这样不仅信息的安全性得不到保障，而且会造成同一网段中信息的泛滥。因此，广播方式不利于与特定对象进行数据交互，并且还浪费了大量的带宽。<br>
+
+   ![在这里插入图片描述](https://img-blog.csdnimg.cn/7c0ed6cea4f14e00b3a07e413dfe7ab5.png#pic_center)
+
+
+
+* **组播** <br>
+综上所述，传统的单播和广播的通信方式均不能以最小的网络开销实现单点发送、多点接收的问题，IP组播技术的出现及时解决了这个问题。
+如下图所示，当IP网络中的某些主机（即Receiver）需要信息时，若采用组播的方式，组播源（即Source）仅需发送一份信息，借助组播路由协议建立组播分发树，被传递的信息在距离组播源尽可能远的网络节点才开始复制和分发。假设只有Host B、Host D和Host E需要信息，采用组播方式时，可以让这些主机加入同一个组播组（Multicast group），组播源向该组播组只需发送一份信息，并由网络中各路由器根据该组播组中各成员的分布情况对该信息进行复制和转发，最后该信息会准确地发送给Host B、Host D和Host E。相比单播来说，组播的优势在于：由于被传递的信息在距信息源尽可能远的网络节点才开始被复制和分发，所以用户的增加不会导致信息源负载的加重以及网络资源消耗的显著增加。相比广播来说，组播的优势在于：由于被传递的信息只会发送给需要该信息的接收者，所以不会造成网络资源的浪费，并能提高信息传输的安全性；另外，广播只能在同一网段中进行，而组播可以实现跨网段的传输。组播的缺点：由于无线组播报文的发送没有相应的确认机制（数据链路层ACK），因此设备无法对丢失的报文进行重传，导致链路质量差的情况下，无线环境下组播报文丢失严重。<br>
+
+   ![在这里插入图片描述](https://img-blog.csdnimg.cn/0a1e5789e3fe4e148509c88013691efa.png#pic_center)
+
+
+
+
+
+* 音视频组播发送端：
+作为组播源将自身音视频组播发送。
+
+* 音视频组播接收端：
+接收音视频组播流。
+
+* FEC：
+前向纠错技术，通过增加冗余带宽方式，提高网络对于丢包的抵抗力。
+
+* FEC相关参数：
+FEC相关参数包括FEC冗余度方法、FEC上行冗余度、FEC min group组大小、FEC max group组大小。FEC冗余度方法包括：固定冗余度（本SDK仅支持固定冗余度）、自动冗余度两种。固定冗余度即全程使用用户指定的冗余度进行FEC编码，自动冗余度则以用户指定的FEC冗余度为基础，根据网络情况进行调整，对于网络较好的场合使用低冗余尽量降低带宽。FEC是分组进行的，即多个拆分包组成一个group，产生其冗余包。group越大，同样冗余度的情况下，产生的冗余包越多，抵抗连续丢包能力越强，同时因丢包产生的抖动也会越大（因为FEC的group可能产生跨多帧的情况，前面包的丢失不得不等到后续包的到来才能恢复）；FEC分组越大消耗CPU资源也越大。建议配置FEC min group大小16；建议根据芯片处理性能设置FEC max group，在性能足够的设备上建议设置为64（PC、主流Android手机均可设置为64，嵌入式平台则根据实际情况设置）。
+
+* NACK：
+重传请求机制，与FEC配合，在预估FEC无法恢复时触发接收端到发送端的重传请求，发送端予以重发响应。NACK仅重传一次，不保证数据一定传输成功，优点是可以获得稳定的低延时，缺点是画面仍可能因丢包而卡顿。
+
+* 组播NACK支持：
+组播是一种单向的传输技术，正常情况下无法使用NACK重传机制，因为每一个接收端都可能有不同的丢包情况。我们使用单播作为组播的一种补充，从而实现了为不同组播接收端提供个性化的NACK服务。
+![在这里插入图片描述](https://img-blog.csdnimg.cn/a462ec49467a482b9dd78bf0e5af6f24.png#pic_center)
+
+* Smooth平滑：
+发送端对一帧较大的码流在时间窗口内平滑发送，避免一次性发出给网络带来的压力。
+
+* JitterBuff：
+接收端为了抵消网络传输、丢包恢复、NACK重传引入的抖动，引入JitterBuff缓存。缓存时间越大，画面流畅度越高，但延时也同步增大。当需要极低延时时，可设置JitterBuff为0。
+
+* 传输参数：
+本SDK中传输相关参数包括：视频通道的上行FEC冗余度、上行FEC Group分组大小、NACK重传支持、接收端JitterBuff缓存时间。对于音视频组播接收端，同样可以设置上行FEC冗余度、上行FEC Group分组大小，只是没有实际意义(因为不会发送数据，当然也不会进行FEC编码)。
+
+
+
+
+---
+# 二、API接口
+ 所有API接口定义均位于SDTerminalSdk.h文件中。
+
+### 1、系统环境初始化，仅需调用一次
+```c
+void  SDTerminal_Enviroment_Init(const char * outputPath,  int outputLevel)
+参数：
+@param: outputPath：日志文件输出的目录，若目录不存在，SDK将自动创建，支持相对路径或绝对路径。日志对于问题定位非常重要，建议开启。
+@param: outputLevel：日志输出的级别，只有等于或者高于该级别的日志会输出到文件，日志级别有： DEBUG、INFO、WARNING、ERROR、ALARM、FATAL、NONE，当指定为NONE时，将不会生成日志文件。具体见TERMINAL_LOG_OUTPUT_LEVEL定义。
+```
+
+### 2、系统退出时调用一次反初始化
+```c
+void  SDTerminal_Enviroment_Free ()
+```
+
+### 3、创建SDK对象
+```c
+void*  SDTerminal_New(TerminalEncodeParams* ptEncodeParams, RemoteVideoYuvDataCallback pfOutputVideo, RemoteAudioPcmDataCallback pfOutputAudio, void* pObject);
+参数：
+@ ptEncodeParams，音视频编码相关参数，包括编码标准类型、是否采用硬编码、编码码率、编码帧率、编码分辨率、音频采样率、声道数等。
+@ pfOutputVideo，视频接收解码输出回调函数。
+@ pfOutputAudio，音频接收解码输出回调函数。
+@ pObject，输出回调函数透传数据。
+返回值：对象指针，返回NULL表示失败。
+```
+
+### 4、销毁SDK对象
+```c
+void  SDTerminal_Delete(void** ppTerminal);
+参数：
+@ ppTerminal，模块指针的指针
+说明：使用者应该做好与其他API之间的互斥保护
+返回值：无
+
+```
+
+### 5、准备会话
+```c
+BOOL  SDTerminal_Online(
+void* pTerminal,
+TerminalLogonParams* ptLogonParams, 
+TerminalTransParams* ptTransParams);
+参数：
+@ pTerminal，模块指针
+@ ptLogonParams，建立连接相关参数，包括客户端类型、组播IP、组播端口、本地IP、组播NACK端口。本地IP的指定用于避免在多网卡时系统默认IP不可控的问题。组播NACK端口用于单播方式传输NACK信令与媒体包，需配置为与组播源一致端口。
+@ ptTransParams，音视频传输相关参数，包括FEC相关、接收缓存相关等。
+返回值：
+返回TRUE表示成功，返回FALSE则为失败。
+
+```
+
+### 6、结束会话
+```c
+void  SDTerminal_Offline(void* pTerminal);
+参数：
+@ pTerminal，模块指针
+返回值：无
+
+```
+### 7、发送视频数据
+```c
+void  SDTerminal_SendVideoData(void* pTerminal, unsigned char* buf, unsigned int unWidth, unsigned int unHeight, TERMINAL_VIDEO_INPUT_FORMAT eFormat);
+参数：
+@ pTerminal，模块指针
+@ buf，输入图像存放区。
+@ unWidth，输入图像宽度，当与编码宽高不一致时，内部自行缩放。建议与编码宽高一致。
+@ unHeight，输入图像高度，当与编码宽高不一致时，内部自行缩放。建议与编码宽高一致。
+@ eFormat, 输入图像格式，建议与编码器当前使用的格式一致。
+说明：若采用硬编码，外层可先通过SDTerminal_GetHardwareEncInfo接口获得当前硬编码支持的输入格式，以此作为外层采集格式。这样避免内部再进行色度空间转换、缩放处理，提高性能。
+返回值：无
+
+```
+
+### 8、发送音频数据
+```c
+void  SDTerminal_SendAudioData(void* pTerminal, unsigned char* buf, unsigned int unLen);
+参数：
+@pTerminal，模块指针
+@ buf，输入音频PCM存放区。
+@ unLen，输入音频PCM字节数。
+返回值：无
+
+```
+
+
+### 9、获取当前SDK版本信息
+```c
+unsigned int SDTerminal_GetVersion (void* pTerminal);
+参数：
+@ pTerminal，模块指针
+返回值：获得当前SDK的版本信息
+
+```
+
+
+---
+
+# 相关资源
+组播纯传输SDK（无内置编解码）:  [https://github.com/waterfoxfox/MulticastSDK/](https://github.com/waterfoxfox/MulticastSDK/) 
+
+SDK 商用及定制化、技术支持服务可联系：[http://www.mediapro.cc/](http://www.mediapro.cc/)
+
---
+++ include/SDTerminalSdk.h
@@ -0,0 +1,408 @@
+﻿//***************************************************************************//
+//* 版权所有  www.mediapro.cc
+//*
+//* 内容摘要：windows 组播 SDT SDK，内置音视频编解码
+//*			  
+//*
+//*	说    明：
+//*			  
+//*				
+//* 当前版本：V1.0		
+//* 作    者：mediapro
+//* 完成日期：2020-8-6
+//**************************************************************************//
+
+#ifndef _SD_MEDIAPRO_TERMINAL_SDK_H_
+#define _SD_MEDIAPRO_TERMINAL_SDK_H_
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if defined _WIN32 || defined __CYGWIN__
+#ifdef DLL_EXPORTS
+#ifdef __GNUC__
+#define DLLIMPORT_MULTICAST_SDK __attribute__ ((dllexport))
+#else
+#define DLLIMPORT_MULTICAST_SDK __declspec(dllexport) 
+#endif
+#else
+#ifdef __GNUC__
+#define DLLIMPORT_MULTICAST_SDK 
+#else
+#define DLLIMPORT_MULTICAST_SDK 
+#endif
+#endif
+#else
+#if __GNUC__ >= 4
+#define DLLIMPORT_MULTICAST_SDK __attribute__ ((visibility ("default")))
+#else
+#define DLLIMPORT_MULTICAST_SDK
+#endif
+#endif
+
+#ifdef WIN32
+	typedef signed __int64       int64_t;
+#endif
+
+#ifdef __APPLE__
+#ifndef OBJC_BOOL_DEFINED
+	typedef int BOOL;
+#endif 
+#else
+#ifndef BOOL
+	typedef int BOOL;
+#endif
+#endif
+
+#ifndef TRUE
+#define TRUE 1
+#endif
+#ifndef FALSE
+#define FALSE 0
+#endif
+
+
+
+//FEC GROUP的最小值、最大值
+#define TERMINAL_FEC_GROUP_MIN			8
+#define TERMINAL_FEC_GROUP_MAX			72
+
+
+//FEC 固定冗余度时可选冗余度的最小值、最大值
+#define TERMINAL_FEC_REDUN_RATIO_MIN	0
+#define TERMINAL_FEC_REDUN_RATIO_MAX	100	
+
+//最大Jitter Buff缓存时长,ms
+#define TERMINAL_JITTER_BUFF_MAX		600
+
+//编码码率最小值、最大值,kbps
+#define TERMINAL_ENC_BITRATE_MIN		50
+#define TERMINAL_ENC_BITRATE_MAX		10000
+
+//编码帧率最小值、最大值
+#define TERMINAL_ENC_FRAMERATE_MIN		1
+#define TERMINAL_ENC_FRAMERATE_MAX		30
+
+//编码分辨率最小值、最大值
+#define	TERMINAL_ENC_WIDTH_MIN		    176
+#define	TERMINAL_ENC_HEIGHT_MIN			144
+#define	TERMINAL_ENC_WIDTH_MAX		    1920
+#define	TERMINAL_ENC_HEIGHT_MAX			1080
+
+//IDR间隔的最小值、最大值
+#define	TERMINAL_ENC_IDR_INTERVAL_MIN	1
+#define	TERMINAL_ENC_IDR_INTERVAL_MAX	10
+
+//音频采样率支持的最小值、最大值
+#define	TERMINAL_SAMPLE_RATE_MIN		8000
+#define	TERMINAL_SAMPLE_RATE_MAX		48000
+
+//音频声道数支持的最小值、最大值
+#define	TERMINAL_AUDIO_CHANNELS_MIN		1
+#define	TERMINAL_AUDIO_CHANNELS_MAX		2
+
+
+
+//日志输出的级别
+typedef enum TERMINAL_LOG_OUTPUT_LEVEL
+{
+	TERMINAL_LOG_OUTPUT_LEVEL_DEBUG		= 1,
+	TERMINAL_LOG_OUTPUT_LEVEL_INFO		= 2,
+	TERMINAL_LOG_OUTPUT_LEVEL_WARNING	= 3,	
+	TERMINAL_LOG_OUTPUT_LEVEL_ERROR		= 4,
+	TERMINAL_LOG_OUTPUT_LEVEL_ALARM		= 5,
+	TERMINAL_LOG_OUTPUT_LEVEL_FATAL		= 6,
+	TERMINAL_LOG_OUTPUT_LEVEL_NONE		= 7
+} TERMINAL_LOG_OUTPUT_LEVEL;
+
+//用户类型
+typedef enum TERMINAL_USER_TYPE
+{
+	MULTICAST_TERMINAL_TYPE_RECV = 0,
+	MULTICAST_TERMINAL_TYPE_SEND = 1
+} TERMINAL_USER_TYPE;
+
+
+//FEC 冗余方法
+typedef enum TERMINAL_FEC_METHOD_TYPE
+{
+	//自动冗余度(目前组播不支持)
+	TERMINAL_FEC_TYPE_AUTO				= 0,
+	//固定冗余度
+	TERMINAL_FEC_TYPE_FIX				= 1,
+} TERMINAL_FEC_METHOD_TYPE;
+
+
+//视频编码图像质量档次，对应X264 X265 preset
+typedef enum TERMINAL_ENCODE_QUALITY
+{
+	//"ultrafast" - baseline
+	TERMINAL_ENC_QUALITY_0				= 0,
+	//"superfast" - baseline
+	TERMINAL_ENC_QUALITY_1				= 1,
+	//"veryfast"  - baseline
+	TERMINAL_ENC_QUALITY_2				= 2,
+	//"faster"    - baseline
+	TERMINAL_ENC_QUALITY_3				= 3,
+	//"fast"      - baseline
+	TERMINAL_ENC_QUALITY_4				= 4,
+	//"medium" - high profile
+	TERMINAL_ENC_QUALITY_5				= 5,
+} TERMINAL_ENCODE_QUALITY;
+
+
+//输入视频格式
+typedef enum TERMINAL_VIDEO_INPUT_FORMAT
+{
+	TERMINAL_VIDEO_FORMAT_INVALID = -1, 
+	TERMINAL_VIDEO_FORMAT_YUV420P,       
+	TERMINAL_VIDEO_FORMAT_NV12,             
+	TERMINAL_VIDEO_FORMAT_NV21,        
+	TERMINAL_VIDEO_FORMAT_RGB24,
+	TERMINAL_VIDEO_FORMAT_RGBA32
+} TERMINAL_VIDEO_INPUT_FORMAT;
+
+
+
+//音视频编码相关参数
+typedef struct TerminalEncodeParams
+{
+	//编码时采用的压缩标准：0-H264  1-H265
+	unsigned int unVideoCodecType;
+	//编码宽度
+	unsigned int unVideoTransWidth;
+	//编码高度
+	unsigned int unVideoTransHeight;
+	//编码帧率
+	unsigned int unVideoFramerate;
+	//编码码率
+	unsigned int unVideoBitrateKbps;
+	//编码质量，越大计算复杂度越高，画质相对更好，取值见：TERMINAL_ENCODE_QUALITY
+	unsigned int unVideoQualityLevelFrom0To5;
+	//编码IDR帧间隔秒数
+	unsigned int unVideoIdrIntervalSecond;
+	//是否启用硬编码，硬编码不支持则自动切回软编码
+	unsigned int bEnableHardwareEnc;
+	//音频编码发送、解码输出的采样率
+	unsigned int unAudioSampleRate;
+	//音频编码发送、解码输出的声道数
+	unsigned int unAudioChannelNum;
+}TerminalEncodeParams;
+
+
+//登录相关参数
+typedef struct TerminalLogonParams
+{
+	//本客户端类型：
+	unsigned int unUserType;
+	//组播端口，将使用本端口发送音频数据  使用本端口+2发送视频数据
+	unsigned int unMulticastPort;
+	//当开启NACK功能时，组播发送端用于NACK信令、媒体传输的单播端口，收发双方保持设置一致时生效。
+	unsigned int unNackPort;
+	//绑定的本地IP地址，当设置为空时，内部将使用INADDR_ANY，交由操作系统选择一个网卡IP。建议指定IP以防多IP时系统选择错误而无法正常收发组播流
+	char acLocalIP[128];
+	//组播地址
+	char acMulticastIP[128];
+}TerminalLogonParams;
+
+
+//传输相关参数
+typedef struct TerminalTransParams
+{
+	//发送使用的FEC方法，取值见：TERMINAL_FEC_METHOD_TYPE
+	unsigned int unFecRedunMethod;
+	//发送使用的FEC冗余度
+	unsigned int unFecRedunRatio;
+	//发送使用的FEC min group size
+	unsigned int unFecMinGroupSize;
+	//发送使用的FEC max group size
+	unsigned int unFecMaxGroupSize;
+	//是否启用组播NACK功能，当设置为TRUE时开启，收发双方保持设置一致时生效。
+	unsigned int bEnableNack;
+	//接收JitterBuff缓存时间，单位毫秒，设置为0时可获得最低延时
+	unsigned int unJitterBuffDelay;
+	//是否启用接收丢包冻结机制，启用后丢包可冻结画面避免用户感知花屏
+	unsigned int bFreezeFrameWhenLost;
+}TerminalTransParams;
+
+
+//输出接收并解码后的YUV数据
+typedef void (*RemoteVideoYuvDataCallback)(unsigned char* data, unsigned int unWidth, unsigned int unHeight, unsigned int unPts, void *pObject);
+
+//输出接收并解码后的PCM数据
+typedef void (*RemoteAudioPcmDataCallback)(unsigned char* data, unsigned int unLen, unsigned int unPts, void *pObject);
+
+
+
+//////////////////////////////////////////////////////////////////////////
+// 客户端类封装接口
+
+/***
+* 环境初始化，系统只需调用一次，主要用于日志模块的初始化
+* @param: outputPath表示日志存放路径，支持相对路径和绝对路径，若目录不存在将自动创建
+* @param: outputLevel表示日志输出的级别，只有等于或者高于该级别的日志输出到文件，取值范围参考TERMINAL_LOG_OUTPUT_LEVEL
+* @return: 
+*/
+DLLIMPORT_MULTICAST_SDK void  SDTerminal_Enviroment_Init(const char* outputPath, int outputLevel);
+
+DLLIMPORT_MULTICAST_SDK void  SDTerminal_Enviroment_Free();
+
+
+/***
+* 创建SDTerminal
+* @param ptEncodeParams: 编码相关参数
+* @param pfOutputVideo: 解码后视频输出回调接口
+* @param pfOutputAudio: 解码后音频输出回调接口
+* @param pObject: 上述输出回调接口的透传指针，将通过回调函数形参方式透传外层
+* @return: 返回模块指针，为NULL则失败
+*/
+DLLIMPORT_MULTICAST_SDK void*  SDTerminal_New(TerminalEncodeParams* ptEncodeParams, RemoteVideoYuvDataCallback pfOutputVideo, 
+								RemoteAudioPcmDataCallback pfOutputAudio, void* pObject);
+
+
+/***
+* 从服务器下线并销毁SDTerminal，使用者应该做好与其他API之间的互斥保护
+* 互斥保护方法1：业务保证其他API接口停止调用后，才会调用本接口
+* 互斥保护方法2：创建两把锁，分别用于音频发送和视频发送等API与本接口的互斥
+*				 一定要对音视频分别使用不同的锁，这样才不会变成二者串行
+* @param ppTerminal: 模块指针指针
+* @return:
+*/
+DLLIMPORT_MULTICAST_SDK void  SDTerminal_Delete(void** ppTerminal);
+
+
+/***
+* 准备推送和接收音视频环境
+* @param pTerminal: 模块指针
+* @param ptLogonParams: 登录相关参数
+* @param ptTransParams: 传输相关参数
+* @return: 接口调用是否成功
+*/
+DLLIMPORT_MULTICAST_SDK BOOL  SDTerminal_Online(void* pTerminal, TerminalLogonParams* ptLogonParams, TerminalTransParams* ptTransParams);
+
+
+/***
+* 停止推送和接收音视频
+* @param pTerminal: 模块指针
+* @return:
+*/
+DLLIMPORT_MULTICAST_SDK void  SDTerminal_Offline(void* pTerminal);
+
+
+/***
+* 编码发送音频
+* @param pTerminal: 模块指针
+* @param buf: 输入音频PCM存放区
+* @param unLen: 输入音频PCM字节数
+* @return:
+*/
+DLLIMPORT_MULTICAST_SDK void  SDTerminal_SendAudioData(void* pTerminal, unsigned char* buf, unsigned int unLen);
+
+
+/***
+* 编码发送视频
+* @param pTerminal: 模块指针
+* @param buf: 待编码发送的视频数据存放区
+* @param unLen: 视频数据存放区字节数
+* @param unWidth: 视频宽度
+* @param unHeight: 视频高度
+* @param eFormat: 视频格式
+* @return:
+*/
+DLLIMPORT_MULTICAST_SDK void  SDTerminal_SendVideoData(void* pTerminal, unsigned char* buf, unsigned int unWidth, unsigned int unHeight, TERMINAL_VIDEO_INPUT_FORMAT eFormat);
+
+
+
+/***
+* 获得硬编码支持情况,Online之后调用生效
+* @param pTerminal: 模块指针
+* @param pbWillUseHwEnc: 返回当前是否采用硬编码
+* @param peHwRecomInputFormat: 若采用硬编码，硬编码推荐的输入格式
+* @return: 接口调用是否成功
+*/
+DLLIMPORT_MULTICAST_SDK BOOL  SDTerminal_GetHardwareEncInfo(void* pTerminal, BOOL* pbWillUseHwEnc, TERMINAL_VIDEO_INPUT_FORMAT *peHwRecomInputFormat);
+
+
+
+/***
+* 获取SDK版本信息
+* @param pTerminal: 模块指针
+* @return: SDK版本号
+*/
+DLLIMPORT_MULTICAST_SDK unsigned int  SDTerminal_GetVersion(void* pTerminal);
+
+
+
+/***
+* 启动上行码流的TS文件录制,Online之后调用生效
+* @param pTerminal: 模块指针
+* @param strTsFileDir: TS文件URL
+* @return: 接口调用是否成功
+*/
+DLLIMPORT_MULTICAST_SDK BOOL  SDTerminal_StartCapUpStream(void* pTerminal, const char* strTsFileUrl);
+
+
+/***
+* 停止上行码流的TS文件录制
+* @param pTerminal: 模块指针
+* @return: 接口调用是否成功
+*/
+DLLIMPORT_MULTICAST_SDK void  SDTerminal_StopCapUpStream(void* pTerminal);
+
+
+/***
+* 启动下行码流的TS文件录制,Online之后调用生效
+* @param pTerminal: 模块指针
+* @param strTsFileDir: TS文件URL
+* @return: 接口调用是否成功
+*/
+DLLIMPORT_MULTICAST_SDK BOOL  SDTerminal_StartCapDownStream(void* pTerminal, const char* strTsFileUrl);
+
+
+/***
+* 停止下行码流的TS文件录制
+* @param pTerminal: 模块指针
+* @return: 
+*/
+DLLIMPORT_MULTICAST_SDK void  SDTerminal_StopCapDownStream(void* pTerminal);
+
+
+/***
+* 启动下行码流的RAW文件录制,Online之后调用生效
+* @param pTerminal: 模块指针
+* @param strRawFileUrl: RAW文件URL
+* @return: 接口调用是否成功
+*/
+DLLIMPORT_MULTICAST_SDK BOOL  SDTerminal_StartCapDownRawStream(void* pTerminal, const char* strRawFileUrl);
+
+
+/***
+* 停止下行码流的RAW文件录制
+* @param pTerminal: 模块指针
+* @return: 
+*/
+DLLIMPORT_MULTICAST_SDK void  SDTerminal_StopCapDownRawStream(void* pTerminal);
+
+
+/***
+* 获取当前时刻音视频上下行丢包率情况，内部已经乘100.0转换为百分比
+* @param pTerminal: 模块指针
+* @param pfVideoUpLostRatio: 视频上行丢包率
+* @param pfVideoDownLostRatio: 视频下行丢包率
+* @param pfAudioUpLostRatio: 音频上行丢包率
+* @param pfAudioDownLostRatio: 音频下行丢包率
+* @return: 
+*/
+DLLIMPORT_MULTICAST_SDK void  SDTerminal_GetVideoAudioUpDownLostRatio(void* pTerminal, float *pfVideoUpLostRatio, float *pfVideoDownLostRatio, 
+														float *pfAudioUpLostRatio, float *pfAudioDownLostRatio);
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // _SD_MEDIAPRO_TERMINAL_SDK_H_
